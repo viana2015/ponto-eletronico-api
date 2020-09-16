3c7d3b167d1c6707444e4b5a2d37bb137d3d1e6e
Cristiano Viana <cristiano.gvianati@gmail.com>
2020-09-15 21:37:08 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/com/jrcg/pontoeletronico/api/PontoeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrcg.pontoeletronico.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
